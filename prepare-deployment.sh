d6651d191bb8717a80c9980f5b94e6ccb989643d
Jonah Butler <jonahbutler6@gmail.com>
2025-07-24 10:53:24 -0400

    deployment prep script

--- prepare-deployment.sh
+++ prepare-deployment.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-# echo "Building Application"
-# npm run build
+echo "Building Application"
+npm run build
 
-# # Force add and stash build directory
-# git add dist -f
-# git stash save
+# Force add and stash build directory
+git add dist -f
+git stash save
 
 # switch to gh-pages
 git checkout gh-pages
-git status
 
 # delete old files
+git rm -rf .
+git stash pop
+ 
+#  copy build dir contents into top level
+cp -a ./dist/. ./
+
+# add and commit
+git add .
+git commit -m "latest build"
+
+# push
+git push origin gh-pages
