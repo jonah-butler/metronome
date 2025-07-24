864e45afee109f99bc8aba93e7d18a0d6bf60d23
Jonah Butler <jonahbutler6@gmail.com>
2025-07-24 10:42:24 -0400

    testing a few things

--- prepare-deployment.sh
+++ prepare-deployment.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "Building Application"
-npm run build
+# echo "Building Application"
+# npm run build
 
-# Force add and stash build directory
-git add dist -f
-git stash save
+# # Force add and stash build directory
+# git add dist -f
+# git stash save
 
 # switch to gh-pages
 git checkout gh-pages
+git status
 
 # delete old files
