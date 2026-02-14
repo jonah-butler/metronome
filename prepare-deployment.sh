4a1f85120d5f9a84e7f614fef4429eeb159b3884
Jonah Butler <jonahbutler6@gmail.com>
2026-02-14 10:58:56 -0500

    latest build

---
+++ prepare-deployment.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Building Application"
+npm run build
+
+# Force add and stash build directory
+git add dist -f
+git stash save
+
+# switch to gh-pages
+git checkout gh-pages
+
+# delete old files
+git rm favicon.svg
+git rm index.html
+git rm -rf assets
+
+# retrieve dist from stash
+git stash pop
+ 
+#  copy build dir contents into top level
+cp -a ./dist/. ./
+
+# delete dist dir
+git rm -rf dist
+
+# add and commit
+git add .
+git commit -m "latest build"
+
+# push
+git push origin gh-pages
+
+# switch
+git checkout -
